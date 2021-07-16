be944312afbb5e2fde685a9cf835b75af804d0e6
rirolli <riccardo.ung@outlook.it>
2021-07-16 18:21:17 +0200

    Modifica di start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,12 @@
 echo "Avvio dei programmi"
 gnome-terminal -t "kafka-console-consumer.sh" -e "$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic twitch" &
 
-## non funziona, non trova il file.
-gnome-terminal -t "view_classifier.py" -e "$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 --master local[*] streaming_jobs/view_classifier.py" # gnome-terminal -t "view_classifier.py" -e "python3 streaming_jobs/view_classifier.py"
+# starting of streaming scripts
+gnome-terminal -t "view_classifier.py" -e "$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 --master local[*] streaming_jobs/view_classifier.py"
+
+# starting of batch scripts
+
 sleep 5
+
+# starting of data producer
 gnome-terminal -t "kafka_producer.py" -e "python3 kafka_producer.py"
