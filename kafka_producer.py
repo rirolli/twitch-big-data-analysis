b12b39fc86247f67a893831691e0f35531171d26
rirolli <riccardo.ung@outlook.it>
2021-07-24 14:13:14 +0200

    Aggiornamento e correzione di kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -3,9 +3,11 @@ import pathlib
 
 from kafka import KafkaProducer
 
-from json import dumps
+from json import dumps, dump, load, loads
 
-from pyspark.sql import SparkSession
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 from os import listdir
 from os.path import isfile, join
@@ -39,42 +41,35 @@ from datetime import datetime
 # [12749764544, [0, "2015-01-20T21:43:45Z", "Music", [75412148, ["dimmak", "-1", 452, "True", "en", 3563, "en", "2014-11-18T00:09:16Z", 1458281, "1920x1080"]]], "2015-02-01T00:05:00Z"]
 
 SAMPLE = True   # True if we want test the project on few samples.
-LIMIT = -1      # We want to limit the number of sources to spped the test of the project. -1 if we don't want limit.
+LIMIT = 2      # We want to limit the number of sources to spped the test of the project. -1 if we don't want limit.
 
 if SAMPLE == False:
     dataset_path = "dataset/twitch_data"
 else:
     dataset_path = "dataset/samples/twitch_data_sample"
 
-def parse_lines(line):
-    line = line.split("\t") # split of the line
-
-    stream_id = int(line[0])
-    current_view = int(line[1])
-    stream_created_time = line[2]
-    game_name = line[3]
-    broadcaster_id = int(line[4])
-    broadcaster_name = line[5]
-    delay_settings = line[6]
-    follower_number = int(line[7])
-    partner_status = line[8]
-    broadcaster_language = line[9]
-    total_view_broadcaster = int(line[10])
-    language = line[11]
-    broadcaster_created_time = line[12]
-    playback_bitrate = int(line[13])
-    source_resolution = line[14]
-    crawl_time = line[15]
-
-    return {'stream_id':stream_id, 'current_view':current_view, 'stream_created_time':stream_created_time,
-    'game_name':game_name, 'broadcaster_id':broadcaster_id, 'broadcaster_name':broadcaster_name,
-    'delay_settings':delay_settings, 'follower_number':follower_number, 'partner_status':partner_status,
-    'broadcaster_language':broadcaster_language, 'total_view_broadcaster':total_view_broadcaster,
-    'language':language, 'broadcaster_created_time':broadcaster_created_time, 'playback_bitrate':playback_bitrate,
-    'source_resolution':source_resolution, 'crawl_time':crawl_time}
+schema = StructType([StructField('stream_id',StringType(),False),
+                    StructField('current_view',IntegerType(),True),
+                    StructField('stream_created_time',TimestampType(),True),
+                    StructField('game_name',StringType(),True),
+                    StructField('broadcaster_id',IntegerType(),False),
+                    StructField('broadcaster_name',StringType(),True),
+                    StructField('delay_settings',StringType(),True),
+                    StructField('follower_number',IntegerType(),True),
+                    StructField('partner_status',StringType(),True),
+                    StructField('broadcaster_language',StringType(),True),
+                    StructField('total_view_broadcaster',IntegerType(),True),
+                    StructField('language',StringType(),True),
+                    StructField('broadcaster_created_time',TimestampType(),True),
+                    StructField('playback_bitrate',IntegerType(),True),
+                    StructField('source_resolution',StringType(),True),
+                    StructField('crawl_time',TimestampType(),False)
+                    ])
 
 def main():
     files_list = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]    # return a list of all the files into the directory we want crawl
+    files_list.sort()
+
     if LIMIT != -1: # We want to limit the number of sources to speed the test of the project 
         files_list = files_list[:LIMIT]
 
@@ -88,23 +83,32 @@ def main():
             .appName("twitch_crawler") \
                 .getOrCreate()
 
+    i = 0   # debug only
     for file in files_list:
         current_file = pathlib.Path(f"{dataset_path}/{file}").parent.resolve()  # get absolute path of file
-        current_file = f"file:///{current_file}"    # adding "file:///" to get local file in spark
+        current_file = f"file:///{current_file}/{file}"    # adding "file:///" to get local file in spark
 
         current_time = f"{file[4:14]}T{file[15:17]}:{file[18:20]}:{file[21:23]}Z"   # get the crawling time from the file name
 
-        input_RDD = spark.sparkContext.textFile(current_file)   # load current file in spark
-        
-        lines = input_RDD \
-            .map(lambda _: _.strip()+f"\t{current_time}") \
-                .map(parse_lines) \
-                    .filter(lambda x: x is not None) \
-                        .collect()
+        input_df = spark \
+            .read \
+                .csv(current_file, schema=schema, sep='\t', nullValue='-1') \
+                    .withColumn('crawl_time', lit(current_time)) \
+                        .filter(col('crawl_time')==current_time)
+
+        input_df.show()
+
+        lines = input_df.toJSON().collect()
 
         for line in lines:
-            producer.send('twitch', value=line)
+            producer.send('twitch', value=loads(line))
             
+
+        # Uncomment only in debug
+        # with open(f"output_{i}.json", 'w') as f:
+        #     dump(lines, f, indent=4)
+        # i += 1
+
         sleep(5)    # sleep to simulate the crawling
         
 
